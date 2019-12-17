f343122dc5d8c8c468b09012211f7df80d3936a8
z\x00^@%00U+2400 <54753063+FULLSHADE@users.noreply.github.com>
2019-12-17 02:39:49 -0500

    Update MEMJECT-COMPACT.py

--- MEMJECT-COMPACT.py
+++ MEMJECT-COMPACT.py
@@ -1,6 +1,6 @@
-import sys # This is the most compact version of a DLL injector with CLI arguments I can write
+import sys
 from ctypes import *
-l = windll.kernel32.OpenProcess(( 0x00F0000 | 0x00100000 | 0xFFF ), False, int(sys.argv[2]))
-a = windll.kernel32.VirtualAllocEx(l, 0, len(sys.argv[1]), ( 0x1000 | 0x2000 ), 0x04)
-w = windll.kernel32.WriteProcessMemory(l, a, sys.argv[1], len(sys.argv[1]), byref(c_int(0)))
-c = windll.kernel32.CreateRemoteThread(l, None, 0,windll.kernel32.GetProcAddress(windll.kernel32.GetModuleHandleA("kernel32.dll"), "LoadLibraryA"), a, 0, byref(c_ulong(0)))
+l=windll.kernel32.OpenProcess(983040|1048576|4095,False,int(sys.argv[2]))
+a=windll.kernel32.VirtualAllocEx(l,0,len(sys.argv[1]),4096|8192,4)
+w=windll.kernel32.WriteProcessMemory(l,a,sys.argv[1],len(sys.argv[1]),byref(c_int(0)))
+c=windll.kernel32.CreateRemoteThread(l,None,0,windll.kernel32.GetProcAddress(windll.kernel32.GetModuleHandleA('kernel32.dll'),'LoadLibraryA'),a,0,byref(c_ulong(0)))
